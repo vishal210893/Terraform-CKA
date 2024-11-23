a3defa7d75fd4e26de37e9881afa873bc49bf0e4
viskumar <viskumar@guidewire.com>
2024-11-24 02:43:01 +0530

    modified code

--- lesson-07/solution/main.tf
+++ lesson-07/solution/main.tf
@@ -17,6 +17,6 @@ resource "aws_instance" "lesson_07" {
   ami           = "ami-0c7c4e3c6b4941f0f"
   instance_type = "t2.micro"
   tags = {
-    Name      = var.instance_name
+    Name = var.instance_name
   }
 }
